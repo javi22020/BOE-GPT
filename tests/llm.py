ee96ce7cd8a6ea7680b732c4a8e0e6cce22aabfd
javi22020 <90896750+javi22020@users.noreply.github.com>
2024-08-23 01:28:19 +0200

    Update Dockerfiles to set CUDA_HOME environment variable and remove unused ccache package

--- tests/llm.py
+++ tests/llm.py
@@ -2,14 +2,15 @@ import openai
 
 client = openai.OpenAI(base_url="http://localhost:4550/v1")
 
-resp = client.chat.completions.create(
+for r in client.chat.completions.create(
     model="phi-3.5-mini-instruct",
     messages=[
         {
             "role": "user",
-            "content": "What is the meaning of life?"
+            "content": "Write a poem"
         }
-    ]
-)
-
-print(resp)
+    ],
+    stream=True
+):
+    if r.choices[0].delta.content is not None:
+        print(r.choices[0].delta.content, end="")
