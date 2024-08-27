cdaa772775ed7a1c75fa39509ef3c13cb8688924
javi22020 <90896750+javi22020@users.noreply.github.com>
2024-08-28 01:22:05 +0200

    modified:   boe_chain/chain.py

--- boe_chain/chain.py
+++ boe_chain/chain.py
@@ -9,14 +9,19 @@ from langchain.chains.retrieval import create_retrieval_chain
 
 class BOEGPTChain():
     def __init__(self) -> None:
+        base_url = "http://localhost:4550"
         api_key="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"
-        self.client = openai.OpenAI(api_key=api_key)
+        self.client = openai.OpenAI(
+            base_url=base_url,
+            api_key=api_key
+        )
         self.llm = ChatOpenAI(
+            base_url=base_url, # Comentar para usar el servidor de OpenAI
             api_key=api_key,
-            model="gpt-4o-mini",
+            model="phi-3.5-mini-instruct",
             streaming=True
         )
-        self.chroma = Chroma(client=HttpClient(host="127.0.0.1", port=8000), collection_name="docs", embedding_function=LlamaCPPEmbeddings())
+        self.chroma = Chroma(client=HttpClient(host="chroma", port=8000), collection_name="docs", embedding_function=LlamaCPPEmbeddings())
         prompt_docs = PromptTemplate.from_template(open("prompt_docs.md", "r", encoding="utf-8").read())
         self.doc_chain = create_stuff_documents_chain(
             llm=self.llm,
